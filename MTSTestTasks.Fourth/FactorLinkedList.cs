a93131d55f267784b0e675e1148228a9ed3eb8d6
Gorddd <kasual2014@icloud.com>
2022-10-18 18:24:34 +0300

    add factor sorted list

---
+++ MTSTestTasks.Fourth/FactorLinkedList.cs
@@ -0,0 +1,71 @@
+﻿namespace MTSTestTasks.Fourth;
+
+internal class FactorSortedList
+{
+    private Node First { get; set; }
+
+    private Node Last { get; set; }
+
+    private int currentMax;
+
+    public IEnumerable<int> InsertAndTake(int item)
+    {
+        if (item <= currentMax)
+            Insert(item);
+        else
+        {
+            int offset = item - currentMax;
+
+            foreach (var itemToReturn in Take(offset))
+                yield return itemToReturn;
+
+            Insert(item);
+        }
+    }
+
+    private void Insert(int value)
+    {
+        if (First == null)
+        {
+            First = Last = new Node(value);
+            return;
+        }
+
+        var currentNode = First;
+        while (currentNode.Next != null)
+        {
+            if (currentNode.Next.Value > value)
+                break;
+            currentNode = currentNode.Next;
+        }
+
+        var newNode = new Node(value) { Next = currentNode.Next };
+        currentNode.Next = newNode;
+        
+        if (Last == currentNode)
+            Last = newNode;
+    }
+
+    private IEnumerable<int> Take(int amount)
+    {
+        var currentNode = First;
+        for (int i = 0; i < amount && currentNode != null; i++)
+        {
+            yield return currentNode.Value;
+            currentNode = currentNode.Next;
+        }
+        First = currentNode;
+    }
+
+    class Node
+    {
+        public int Value { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public Node Next { get; set; } = null!;
+    }
+}
---
+++ MTSTestTasks.Fourth/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MTSTestTasks.Fourth;
+
+class Program
+{
+    public static void Main()
+    {
+        var collection = Sort(new[] { 1, 0, 4, 6, 2, 8, 5 }, 4, 8);
+        foreach (var item in collection)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    public static IEnumerable<int> Sort(IEnumerable<int> inputStream, int sortFactor, int maxValue)
+    {
+        var linkedList = new FactorSortedList();
+        foreach (var item in inputStream)
+        {
+            
+        }
+        
+    }
+
+    private static void InsertInSortedLinkedList(LinkedList<int> linkedList, int value)
+    {
+        LinkedListNode<int> currentNode = linkedList.First;
+        if (currentNode == null)
+        {
+            linkedList.AddLast(value);
+            return;
+        }
+
+        while(currentNode != null)
+        {
+            if (currentNode.Value > value)
+                break;
+            currentNode = currentNode.Next;
+        }
+
+        linkedList.AddBefore(currentNode, value);
+    }
+}
