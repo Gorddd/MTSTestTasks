217874e071d6a245ef71c8677e847ede5191e353
Gorddd <kasual2014@icloud.com>
2022-10-19 23:43:31 +0300

    add fifth solution

---
+++ MTSTestTasks.Fifth/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TransformToElephant();
+        Console.WriteLine("Муха");
+        //... custom application code
+        Console.WriteLine("Hello how low");
+    }
+
+    static void TransformToElephant()
+    {
+        Console.WriteLine("Слон");
+        Console.SetOut(new SkipOneWriter());
+    }
+
+    class SkipOneWriter : TextWriter
+    {
+        private TextWriter standardOutStream;
+
+        public override Encoding Encoding => Encoding.UTF8;
+
+        public SkipOneWriter()
+        {
+            standardOutStream = Console.Out;
+        }
+
+        public override void WriteLine(string value)
+        {
+            Console.SetOut(standardOutStream);
+        }
+    }
+}
